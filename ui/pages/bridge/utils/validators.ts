86d156cbab4ceaf8327ee74c2571a5144b9a6519
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-10 17:59:21 -0400

    chore: add tests for bridge status validators

--- ui/pages/bridge/utils/validators.ts
+++ ui/pages/bridge/utils/validators.ts
@@ -17,6 +17,9 @@ export const validateResponse = <ExpectedResponse>(
   data: unknown,
   urlUsed: string,
 ): data is ExpectedResponse => {
+  if (data === null || data === undefined) {
+    return false;
+  }
   return validateData(validators, data, urlUsed);
 };
 
