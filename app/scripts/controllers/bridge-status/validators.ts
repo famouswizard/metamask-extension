04762ff1504b99b6f97e5f88d879d055faf5e305
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-23 11:46:52 -0400

    fix: not being able to fetch pending bridge status, should not cache status fetch

--- app/scripts/controllers/bridge-status/validators.ts
+++ app/scripts/controllers/bridge-status/validators.ts
@@ -1,7 +1,14 @@
 import { validHex } from '../../../../shared/lib/swaps-utils';
 import { isValidHexAddress } from '../../../../shared/modules/hexstring-utils';
 import { validateResponse } from '../../../../ui/pages/bridge/bridge.util';
-import { Asset, BridgeId, ChainStatus, StatusTypes } from './types';
+import {
+  Asset,
+  BridgeId,
+  DestChainStatus,
+  SrcChainStatus,
+  StatusTypes,
+} from './types';
+import { BRIDGE_STATUS_BASE_URL } from './utils';
 
 const assetValidators = [
   {
@@ -38,9 +45,9 @@ const assetValidators = [
 ];
 
 const assetValidator = (v: unknown): v is Asset =>
-  validateResponse<Asset, unknown>(assetValidators, v, 'dummyurl.com');
+  validateResponse<Asset, unknown>(assetValidators, v, BRIDGE_STATUS_BASE_URL);
 
-const chainStatusValidators = [
+const srcChainStatusValidators = [
   {
     property: 'chainId',
     // For some reason, API returns destChain.chainId as a string, it's a number everywhere else
@@ -66,11 +73,48 @@ const chainStatusValidators = [
   },
 ];
 
-const chainStatusValidator = (v: unknown): v is ChainStatus =>
-  validateResponse<ChainStatus, unknown>(
-    chainStatusValidators,
+const srcChainStatusValidator = (v: unknown): v is SrcChainStatus =>
+  validateResponse<SrcChainStatus, unknown>(
+    srcChainStatusValidators,
     v,
-    'dummyurl.com',
+    BRIDGE_STATUS_BASE_URL,
+  );
+
+const destChainStatusValidators = [
+  {
+    property: 'chainId',
+    // For some reason, API returns destChain.chainId as a string, it's a number everywhere else
+    type: 'number|string',
+    validator: (v: unknown): v is number | string =>
+      typeof v === 'number' || typeof v === 'string',
+  },
+  {
+    property: 'amount',
+    type: 'string|undefined',
+    validator: (v: unknown): v is string | undefined =>
+      typeof v === 'string' || v === undefined,
+  },
+  {
+    property: 'txHash',
+    type: 'string|undefined',
+    validator: (v: unknown): v is string | undefined =>
+      typeof v === 'string' || v === undefined,
+  },
+  {
+    property: 'token',
+    type: 'object|undefined',
+    validator: (v: unknown): v is Asset | undefined =>
+      v === undefined ||
+      (v && typeof v === 'object' && Object.keys(v).length === 0) ||
+      assetValidator(v),
+  },
+];
+
+const destChainStatusValidator = (v: unknown): v is DestChainStatus =>
+  validateResponse<DestChainStatus, unknown>(
+    destChainStatusValidators,
+    v,
+    BRIDGE_STATUS_BASE_URL,
   );
 
 export const validators = [
@@ -83,12 +127,12 @@ export const validators = [
   {
     property: 'srcChain',
     type: 'object',
-    validator: chainStatusValidator,
+    validator: srcChainStatusValidator,
   },
   {
     property: 'destChain',
     type: 'object',
-    validator: chainStatusValidator,
+    validator: destChainStatusValidator,
   },
   {
     property: 'bridge',
--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2135,10 +2135,9 @@ export default class MetamaskController extends EventEmitter {
           'NetworkController:getSelectedNetworkClient',
           'NetworkController:findNetworkClientIdByChainId',
         ],
-        allowedEvents: ['TransactionController:transactionConfirmed'],
+        allowedEvents: [],
       });
     this.bridgeStatusController = new BridgeStatusController({
-      provider: this.provider,
       messenger: bridgeStatusControllerMessenger,
     });
 
