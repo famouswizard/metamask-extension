84a65370ea5d498019022e691e61433957d5a59c
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-26 13:26:20 -0400

    chore: wipe bridge status on clear activity tab data

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2132,8 +2132,9 @@ export default class MetamaskController extends EventEmitter {
         name: BRIDGE_STATUS_CONTROLLER_NAME,
         allowedActions: [
           'AccountsController:getSelectedAccount',
-          'NetworkController:getSelectedNetworkClient',
+          'NetworkController:getNetworkClientById',
           'NetworkController:findNetworkClientIdByChainId',
+          'NetworkController:getState',
         ],
         allowedEvents: [],
       });
@@ -4902,6 +4903,10 @@ export default class MetamaskController extends EventEmitter {
       this.accountsController.getSelectedAccount().address;
     this.txController.wipeTransactions(false, selectedAddress);
     this.smartTransactionsController.wipeSmartTransactions({
+      address: selectedAddress,
+      ignoreNetwork: false,
+    });
+    this.bridgeStatusController.wipeBridgeStatus({
       address: selectedAddress,
       ignoreNetwork: false,
     });
